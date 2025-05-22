2701b79e3c311f210e9a283cbeed6deaa47b6924
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-05-22 13:58:57 +0300

    Add splash screen implementation with routing
    - Created a new SVG asset for the splash screen background.
    - Implemented the onGenerateRoute function to handle navigation to the SplashView.
    - Developed the SplashView widget as the entry point for the splash screen.
    - Added a SplashViewBody widget to structure the content of the splash screen.

---
+++ lib/core/helper/on_generate_route_function.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hup/features/splash/presentation/views/splash_view.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings setting) {
+  switch (setting.name) {
+    case SplashView.id:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+    default:
+      return MaterialPageRoute(
+        builder:
+            (context) =>
+                const Scaffold(body: Center(child: Text('Route not found'))),
+      );
+  }
+}
